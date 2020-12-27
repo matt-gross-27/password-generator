b8ce828f3689948fe22e5f183e58a440f301d0a2
Matt Gross <mbgross111@gmail.com>
2020-12-27 15:52:11 -0800

    pull remote main branch

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Create an object that holds password char types 
+let charOptions = {
+  alpha: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  numeric: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  special: ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}" ,"~" ,"]" ]
+}  
+
+console.log(String.password())
+
+
+/*~~~~~~~~~~~~~~~SUMMARY~~~~~~~~~~~~~~~*
+  GIVEN I need a new, secure password
+  WHEN I click the button to generate a password
+  THEN I am presented with a series of prompts for password criteria
+  WHEN prompted for password criteria
+  THEN I select which criteria to include in the password
+  WHEN prompted for the length of the password
+  THEN I choose a length of at least 8 characters and no more than 128 characters
+  WHEN prompted for character types to include in the password
+  THEN I choose lowercase, uppercase, numeric, and/or special characters
+  WHEN I answer each prompt
+  THEN my input should be validated and at least one character type should be selected
+  WHEN all prompts are answered
+  THEN a password is generated that matches the selected criteria
+  WHEN the password is generated
+  THEN the password is either displayed in an alert or written to the page
+*~~~~~~~~~~~~~~~~SUMMARY~~~~~~~~~~~~~~~*/

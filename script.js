04dfb9deecdc00e2b298806ad6d3ec4862f91741
Matt Gross <mbgross111@gmail.com>
2020-12-27 21:23:22 -0800

    working version 1 finished

--- script.js
+++ script.js
@@ -1,14 +1,116 @@
+/*~~~~~~~~~~~~~~~CHECK LIST~~~~~~~~~~~~~~~*
+  GIVEN I need a new, secure password:
+  
+  WHEN I click the button to generate a password
+  THEN I am presented with a series of prompts for password criteria
+    -done
+  
+  WHEN prompted for password criteria
+  THEN I select which criteria to include in the password
+    -done
+  
+  WHEN prompted for the length of the password
+  THEN I choose a length of at least 8 characters and no more than 128 characters
+    -done
+  
+  WHEN prompted for character types to 'INCLUDE' in the password 
+  THEN I choose lowercase, uppercase, numeric, and/or special characters
+    -as of now: character types are 'allowed' NOT 'included' 
+    -each character type won't always be included if 2 or more types are selected
+    -the longer the password the more likely each type will be included
+  
+  WHEN I answer each prompt
+  THEN my input should be validated and at least one character type should be selected
+    -each prompt is not validated only one type included is validated
+  
+  WHEN all prompts are answered
+  THEN a password is generated that matches the selected criteria
+    -done
+  
+  WHEN the password is generated
+  THEN the password is either displayed in an alert or written to the page
+    -done
+*~~~~~~~~~~~~~~~~CHECK LIST~~~~~~~~~~~~~~~*/
+
+let characters = {
+  alphaLower: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  alphaUpper: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  numeric: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  special: ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}" ,"~"]
+};
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Generate Password
+var generatePassword = function(){
+  //reset password and chosen characters before regenerating
+  var randomString = "";
+  var userCharacters = [];
+  var alphaLower = false;
+  var alphaUpper = false;
+  var numeric = false;
+  var special = false;
+
+
+  // ask for password length
+  var promptLength = window.prompt("Please choose a password length.\nEnter a number between 8 and 128");
+  
+  //convert string to int
+  promptLength = parseInt(promptLength);
+  
+  //test if between 8 and 128
+  if (promptLength >= 8 && promptLength <= 128) {
+  
+    alphaLower = window.confirm ("allow lowercase characters? abc...")
+    if(alphaLower) {
+      userCharacters = userCharacters.concat(characters.alphaLower);
+    }
+    alphaUpper = window.confirm ("allow uppercase characters? ABC...")
+    if(alphaUpper) {
+      userCharacters = userCharacters.concat(characters.alphaUpper);
+    }
+    numeric = window.confirm ("allow numeric characters? 123...")
+    if(numeric) {
+      userCharacters = userCharacters.concat(characters.numeric);
+    }
+    special = window.confirm ("allow special characters? !@#...")
+    if(special) {
+      userCharacters = userCharacters.concat(characters.special);
+      }
+    console.log(userCharacters);
+
+      if( alphaLower === false &&
+        alphaUpper === false &&
+        numeric === false &&
+        special === false ) {
+        window.alert("passwords need characters... try again");
+        return generatePassword();
+    }
+    // for loop to generate characters for length of password
+    for (var i = 0; i < promptLength; i++) {
+      //create a random character
+      var randomChar = userCharacters[Math.floor(Math.random() * userCharacters.length)];
+      //update a radom string using each random character[i]
+      randomString = randomString + randomChar;
+    }
+    return randomString;
+  }
+  else {
+    window.alert("please enter a valid answer");
+    return generatePassword();
+  }
+};
+ 
+// write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
+};
 
-}
-
-// Add event listener to generate button
+// clicking the generateBtn calls function writePassword()
 generateBtn.addEventListener("click", writePassword);

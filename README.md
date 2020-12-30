adf6159dafce35ceeb3b35d221aa42f3b6d4448a
Matt Gross <mbgross111@gmail.com>
2020-12-29 21:38:38 -0800

    update README

--- README.md
+++ README.md
@@ -1,48 +1,58 @@
-# 03 JavaScript: Password Generator
+# JavaScript: Password Generator
 
-Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+The application generates a random password that matches user criteria and then prints the result in a textarea element.
+</br>
+</br>
 
-If you are unfamiliar with special characters, take a look at [some examples](https://www.owasp.org/index.php/Password_special_characters).
-
-## User Story
-
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
-
-## Acceptance Criteria
-
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
-
-The following image demonstrates the application functionality:
+## How It Looks:
 
 ![password generator demo](./Assets/03-javascript-homework-demo.png)
+</br>
+</br>
+## Links
+Checkout: [Deployed Application Here](https://matt-gross-27.github.io/password-generator/)
 
-## Review
+Checkout: [Git Hub Repo Here](https://github.com/matt-gross-27/password-generator)
+</br>
+</br>
 
-You are required to submit the following for review:
+## How It Works:
 
-* The URL of the deployed application.
+`1.` Click the Generate Password button to run `writePassword()`
 
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+`2.` `writePassword()` sets 'password' equal to the result of step 3
 
-- - -
-© 2020 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+`3.` `generatePassword()`
+
+1. Create a blank array: `passwordArray`
+
+2. Prompt user to specify password length between 8 and 128 characters and store result as `promptLength`
+
+3. Repeat previous step until answer is valid
+
+4. `selectCharacters()` 
+    1. Ask user if they would like to include each of the following character types:
+        - Lowercase (abc...)
+        - Uppercase (ABC...)
+        - Numeric (123...)
+        - Special (!@#...)
+
+    2. Store user selections and set array `use.characters` to contain all characters of only the selected types.
+
+    3. Repeat `selectCharacters()` until user chooses at least one character type, confirms her choices.
+
+5. For `promptLength` less the number of selected character types:
+
+    1. Randomly select a character from `use.characters`
+
+    2. Insert that character to the end of the `passwordArray`
+
+    3. Require selected characters are used by adding one character to the end of `passwordArray` from each selected type
+
+6. [Fisher-Yates Shuffle](https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle) `passwordArray`
+```
+this randomly locates the required characters added to end of the `password array` in step 5.3
+```
+7. Convert the `passwordArray` to a Sting and return value to `generatePassword()`
+
+`4.` Display the password to user in a textarea
--- script.js
+++ script.js
@@ -17,7 +17,7 @@ var use = {
   special: false,
   countCharacterSets: 0,
   characters: [],
-  reset: function() {
+  reset: function () {
     this.alphaLower = false;
     this.alphaUpper = false;
     this.numeric = false;
@@ -41,7 +41,7 @@ var selectCharacters = function () {
       //add lowercase characters to use.characters array
       use.characters = use.characters.concat(characters.alphaLower);
       selectedCharactersAlert += '  - lowercase (abc...)\n';
-      use.countCharacterSets ++;
+      use.countCharacterSets++;
     }
     // ask user: include uppercase?
     use.alphaUpper = window.confirm("Include uppercase characters? (ABC...)");
@@ -49,7 +49,7 @@ var selectCharacters = function () {
       //add uppercase characters to var use.characters array
       use.characters = use.characters.concat(characters.alphaUpper);
       selectedCharactersAlert += '  - uppercase (ABC...)\n';
-      use.countCharacterSets ++;
+      use.countCharacterSets++;
     }
     // ask user: include numeric?
     use.numeric = window.confirm("Include numeric characters? (123...)");
@@ -57,7 +57,7 @@ var selectCharacters = function () {
       //add numeric characters to var use.characters array
       use.characters = use.characters.concat(characters.numeric);
       selectedCharactersAlert += '  - numeric (123...)\n';
-      use.countCharacterSets ++;
+      use.countCharacterSets++;
     }
     // ask user: include special?
     use.special = window.confirm("Include special characters? (!@#...)");
@@ -65,7 +65,7 @@ var selectCharacters = function () {
       // add special characters to var use.characters array
       use.characters = use.characters.concat(characters.special);
       selectedCharactersAlert += '  - special (!@#...)\n';
-      use.countCharacterSets ++;
+      use.countCharacterSets++;
     }
     // if no characters were selected alert user they need to select some and start while loop again
     if (!use.alphaLower && !use.alphaUpper && !use.numeric && !use.special) {
@@ -76,7 +76,7 @@ var selectCharacters = function () {
       var confirmCharacters = window.confirm(
         "You have selected the following character types:\n" +
         selectedCharactersAlert +
-        "Would you like to generate your password?");
+        "Would you like to generate your password? Select OK \nWant to change your characters? Select Cancel");
       // if they do not confirm, reset characters which will keep them in while loop
       if (!confirmCharacters) {
         use.reset();
@@ -85,73 +85,73 @@ var selectCharacters = function () {
   }
 }
 // Generate Password Function
-  var generatePassword = function () {
-    //reset password
-    var passwordArray = [];
-    // ask for password length
-    var promptLength = window.prompt("Please choose a password length.\nType a number between 8 and 128.");
-    // convert string to int
-    promptLength = parseInt(promptLength);
-    // test if user response is between 8 and 128
-    if (promptLength >= 8 && promptLength <= 128) {
-      // if true ask what types of characters to include
-      selectCharacters();
-      // for the length of password - 1 for each 'required' character type...
-      for (var i = 0; i < promptLength - use.countCharacterSets; i++) {
-        // choose a random character from all selected character sets...
-        var randomCharacter = use.characters[Math.floor(Math.random() * use.characters.length)];
-        // and add that character to passwordArray.
-        passwordArray.push(randomCharacter);
-      }
-      // if user selected lowercase
-      if(use.alphaLower) {
-        // add 1 lowercase character to passwordArray to make sure at least one makes it in.
-        var addOn = characters.alphaLower[Math.floor(Math.random() * characters.alphaLower.length)];
-        passwordArray.push(addOn);
-      }
-      // if user selected uppercase
-      if(use.alphaUpper) {
-        // add 1 uppercase character to passwordArray
-        addOn = characters.alphaUpper[Math.floor(Math.random() * characters.alphaUpper.length)];
-        passwordArray.push(addOn);
-      }
-      // if user selected numeric
-      if(use.numeric) {
-        // add 1 numeric character to passwordArray
-        addOn = characters.numeric[Math.floor(Math.random() * characters.numeric.length)];
-        passwordArray.push(addOn);
-      }
-      // if user selected special
-      if(use.special) {
-        // add 1 special character to passwordArray
-        addOn = characters.special[Math.floor(Math.random() * characters.special.length)];
-        passwordArray.push(addOn);
-      }
-      // "Fisher-Yates Shuffle" the password array so required characters appear in random positions instead of appended onto the end
-      for(var i = passwordArray.length - 1; i > 0; i--) {
-        var            j = Math.floor(Math.random() * i)
-        var  storeToSwap = passwordArray[i]
-        passwordArray[i] = passwordArray[j]
-        passwordArray[j] = storeToSwap
-      }
-      //convert passwordArray to a string
-      var passwordString = passwordArray.join('');
-      //return that string so it can be accessed by writePassword() function's first statement
-      return passwordString;
+var generatePassword = function () {
+  //reset password
+  var passwordArray = [];
+  // ask for password length
+  var promptLength = window.prompt("Please choose a password length.\nType a number between 8 and 128.");
+  // convert string to int
+  promptLength = parseInt(promptLength);
+  // test if user response is between 8 and 128
+  if (promptLength >= 8 && promptLength <= 128) {
+    // if true ask what types of characters to include
+    selectCharacters();
+    // for the length of password - 1 for each 'required' character type...
+    for (var i = 0; i < promptLength - use.countCharacterSets; i++) {
+      // choose a random character from all selected character sets...
+      var randomCharacter = use.characters[Math.floor(Math.random() * use.characters.length)];
+      // and add that character to passwordArray.
+      passwordArray.push(randomCharacter);
     }
-    //else the user didn't choose a length between 8 and 128 - start over
-    else {
-      window.alert("please enter a valid answer");
-      return generatePassword();
+    // if user selected lowercase
+    if (use.alphaLower) {
+      // add 1 lowercase character to passwordArray to make sure at least one makes it in.
+      var addOn = characters.alphaLower[Math.floor(Math.random() * characters.alphaLower.length)];
+      passwordArray.push(addOn);
     }
-  };
+    // if user selected uppercase
+    if (use.alphaUpper) {
+      // add 1 uppercase character to passwordArray
+      addOn = characters.alphaUpper[Math.floor(Math.random() * characters.alphaUpper.length)];
+      passwordArray.push(addOn);
+    }
+    // if user selected numeric
+    if (use.numeric) {
+      // add 1 numeric character to passwordArray
+      addOn = characters.numeric[Math.floor(Math.random() * characters.numeric.length)];
+      passwordArray.push(addOn);
+    }
+    // if user selected special
+    if (use.special) {
+      // add 1 special character to passwordArray
+      addOn = characters.special[Math.floor(Math.random() * characters.special.length)];
+      passwordArray.push(addOn);
+    }
+    // "Fisher-Yates Shuffle" the password array so required characters appear in random positions instead of appended onto the end
+    for (var i = passwordArray.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * i)
+      var storeToSwap = passwordArray[i]
+      passwordArray[i] = passwordArray[j]
+      passwordArray[j] = storeToSwap
+    }
+    //convert passwordArray to a string
+    var passwordString = passwordArray.join('');
+    //return that string so it can be accessed by writePassword() function's first statement
+    return passwordString;
+  }
+  //else the user didn't choose a length between 8 and 128 - start over
+  else {
+    window.alert("please enter a valid answer");
+    return generatePassword();
+  }
+};
 
 // Write Password Function: writes generatePassword result to the #password input
-  function writePassword() {    
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
-  };
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+};
 
 // clicking the generateBtn calls function writePassword()
 generateBtn.addEventListener("click", writePassword);
